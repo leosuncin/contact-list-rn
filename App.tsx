a7ef6381b8653be85820c12786cf9bc4bd10f8a6
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-25 11:20:26 -0600

    feat: Add stack navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import ListContact from 'screens/ListContact';
 import store from 'state/store';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <Provider store={store}>
       <PaperProvider>
-        <ListContact />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="ListContact" headerMode="none">
+            <Stack.Screen name="ListContact" component={ListContact} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </PaperProvider>
     </Provider>
   );
