53b7c7515e44175b719116c3d0a7aa3840db00d5
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-02-25 17:09:10 -0600

    feat: Add form to enter basic info

--- screens/CreateContact/BasicTab.tsx
+++ screens/CreateContact/BasicTab.tsx
@@ -1,11 +1,37 @@
+import TextInput from 'components/TextInput';
 import React from 'react';
 import { NativeModules, Platform, StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Divider, Headline } from 'react-native-paper';
 
 const BasicTab: React.FC = () => {
   return (
     <View style={styles.screen}>
-      <Text>Basic</Text>
+      <Headline>Basic information</Headline>
+      <Divider style={styles.divider} />
+      <TextInput mode="outlined" label="Full name" autoCapitalize="words" />
+      <TextInput
+        mode="outlined"
+        label="Username"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <TextInput
+        mode="outlined"
+        label="e-mail"
+        autoCapitalize="none"
+        keyboardType="email-address"
+      />
+      <TextInput
+        mode="outlined"
+        label="Phone number"
+        keyboardType="phone-pad"
+      />
+      <TextInput
+        mode="outlined"
+        label="Website"
+        autoCapitalize="none"
+        keyboardType="url"
+      />
     </View>
   );
 };
@@ -16,5 +42,8 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: Platform.OS === 'ios' ? 25 : NativeModules.StatusBarManager.HEIGHT,
+  },
+  divider: {
+    marginVertical: 10,
   },
 });
